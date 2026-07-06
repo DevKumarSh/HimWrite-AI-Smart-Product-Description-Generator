6e941f40bf3bc8427963049197567ec0f4c18701
deverror2508 <devsharmakumar74@gmail.com>
2026-07-06 16:04:12 +0530

    Add CURD Features

--- Backend/routes/apiRoutes.js
+++ Backend/routes/apiRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { generateDescription } = require('../controllers/generateController');
-const { getHistory, deleteHistoryItem, clearHistory } = require('../controllers/historyController');
+const { getHistory, deleteHistoryItem, clearHistory, updateHistoryItem } = require('../controllers/historyController');
 
 // POST /api/generate
 router.post('/generate', generateDescription);
 
 // History Routes
 router.get('/history', getHistory);
+router.put('/history/:id', updateHistoryItem);
 router.delete('/history/:id', deleteHistoryItem);
 router.delete('/history', clearHistory);
 
